8f1ef84b70a72126d32e3370ad4bb41b6dd37556
Jonathan Phouminh <jonathanphouminh@Jonathans-MacBook-Pro.local>
2019-09-21 11:49:11 -0600

    scripting lab updated

--- lab3_script.sh
+++ lab3_script.sh
@@ -1,26 +1,27 @@
  #!/bin/bash
 # Authors : Jonathan Phouminh , Zach Chommala 
-# Date: 1/1/2019
+# Date: 09/21/2019
 
 #Problem 1 Code:
 #Make sure to document how you are solving each problem!
-#echo "Input filenname:"  
-#read filename 
-#echo "Input regular expression" 
-#read regex 
-#^^^^ uncomment everything above 
-# count the number of lines that has the 303  expression 
+echo "Input filenname:"  
+read filename 
+echo "Input regular expression" 
+read regex
+ 
+# count the number of lines that has the 303 expression 
 filename="regex_practice.txt" 
 regex="303"
+echo "NUMBER OF PHONE NUMBER MATCHES:" 
+grep -c $regex $filename 
 
 #counting emails by only considering strings containing @ symbol 
-echo "NUMBER OF PHONE NUMBER MATCHES:" 
-grep -c  $regex $filename  
+# -c flag counts the amount of matches    
 echo "NUMBER OF EMAILS: " 
 grep -c "@" $filename
 
 # need to parse the items with that dont have any string characters  
-echo "LIST OF 303 AREA CODES" 
+echo "LIST OF 303 AREA CODE PHONE NUMBERS:" 
 grep "303-"  $filename 
 
 # need to append all emails under the address @geocities.com 
